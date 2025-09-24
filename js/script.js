58811e89b4bed77203c1da605e611c7d6ae868eb
Salvador Camiege <camiegesalvador@gmail.com>
2025-09-24 16:06:30 +0100

    atualiazaçôes no site

--- js/script.js
+++ js/script.js
@@ -27,6 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    
+    
     const form = document.getElementById("inscricaoForm");
 if (form) {
     form.addEventListener("submit", function(event) {
@@ -168,3 +170,21 @@ if (form) {
     window.addEventListener('scroll', handleScrollAnimations);
     handleScrollAnimations();
 });
+
+ const modal = document.getElementById("modal");
+  const openBtn = document.getElementById("openModalBtn");
+  const closeBtn = document.querySelector(".close");
+
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
